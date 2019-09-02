4eb99c8917523f090bd4a5c548dbb684b54ef1e1
LazyyPimp <54757674+LazyyPimp@users.noreply.github.com>
2019-09-02 00:22:29 -0700

    Create meme.js

---
+++ commands/meme.js
@@ -0,0 +1,17 @@
+const config = require('../config.json');
+const Discord = require('discord.js');
+const randomPuppy = require('random-puppy');
+
+module.exports.run = async (bot, message, args) => {
+    randomPuppy('animemes')
+    .then(url => {
+        const embed = new Discord.RichEmbed()
+        .setImage(url)
+        .setColor('#A65EA5')
+     return message.channel.send({ embed });
+})
+}
+
+module.exports.help = {
+    name: "meme"
+}
