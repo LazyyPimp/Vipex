bd5a2841809eb167d7a0a2354776fffaa961a439
LazyyPimp <54757674+LazyyPimp@users.noreply.github.com>
2019-09-02 00:38:59 -0700

    Update birb.js

--- commands/birb.js
+++ commands/birb.js
@@ -9,20 +9,6 @@ let testEmbed = new Discord.RichEmbed()
 .setTitle("title")
 .setImage(`https://imgur.com/${text}`);
 message.channel.send(testEmbed);
-
-
-// using node-fetch
-const fetch = require('node-fetch');
-
-let { file } = await fetch('http://random.birb.pw/tweet.json').then(body => body.json()); // getting "file" object from fetched body
-// console.log(file);
-message.channel.send(`https://imgur.com/${file}`);
-let testEmbed = new Discord.RichEmbed()
-.setColor("0x000000")
-.setTitle("title")
-.setImage(`https://imgur.com/${file}`);
-message.channel.send(testEmbed);
-
 }
 
 module.exports.help = {
