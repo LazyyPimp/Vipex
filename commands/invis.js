578a3ed36504bead03fe08d51ee4c6483f480dea
LazyyPimp <54757674+LazyyPimp@users.noreply.github.com>
2019-09-02 13:51:35 -0700

    Update invis.js

--- commands/invis.js
+++ commands/invis.js
@@ -6,6 +6,8 @@ module.exports.run = async (bot, message, args) => {
 bot.user.setStatus('invisible')
   .then(console.log)
   .catch(console.error);
+    (message.channel.send(`Set status to "invisible"!`).then(msg => msg.delete(5000)));
+
 }
 
 module.exports.help = {
