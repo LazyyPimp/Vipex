3be60f287ad779fc6e72c86a11ccfd3d2aca5f1b
LazyyPimp <54757674+LazyyPimp@users.noreply.github.com>
2019-09-01 22:38:12 -0700

    Update index.js

--- index.js
+++ index.js
@@ -108,7 +108,7 @@ let member = message.mentions.members.first();
 if(!member)
   return message.reply("Please mention a valid member of this server");
 if(!member.bannable) 
-  return message.reply("I cannot ban this user! Do they have a higher role? 
+  return message.reply("I cannot ban this user! Do they have a higher role?") 
 Do I have ban permissions?");
 
 let reason = args.slice(1).join(' ');
