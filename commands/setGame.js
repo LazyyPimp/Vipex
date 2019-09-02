0c4b0d2eca6404fd5b777b8eb849ff7f086c0ced
LazyyPimp <54757674+LazyyPimp@users.noreply.github.com>
2019-09-01 21:27:53 -0700

    Create setGame.js

---
+++ commands/setGame.js
@@ -0,0 +1,21 @@
+
+{
+  "name": "set",
+  "permissions": "NONE",
+  "restriction": "1",
+  "_id": "gEPiw",
+  "actions": [
+    {
+      "info": "1",
+      "infoIndex": "1",
+      "storage": "1",
+      "varName": "status",
+      "name": "Store Command Params"
+    },
+    {
+      "gameName": "${tempVars(\"status\")}",
+      "gameLink": "",
+      "name": "setGame"
+    }
+  ]
+}
