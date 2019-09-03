ca34274ee29f039efe968e6bb904a25462f3e5cb
LazyyPimp <54757674+LazyyPimp@users.noreply.github.com>
2019-09-02 18:29:08 -0700

    Update embed.js

--- commands/embed.js
+++ commands/embed.js
@@ -2,19 +2,41 @@ const Discord = require("discord.js");
 
 module.exports.run = async (bot,message,args) => {
 
-    if(!args[0]) return message.reply("What you want me to embed?")
+// set message listener 
+client.on('message', message => {
+    let command = message.content;
 
-        let usermsg = args.slice(0).join(" ");
+    // match commands like /embed [title]; [description]
+    // first \w+ is for the title, 2nd is for description
+    if ( /^\/embed \[[\w ]+\]; \[[\w ]+\]$/.test(command) )
+        sendEmbed(message);
+});
 
-        let Embedmsg = new Discord.RichEmbed()
-        .setColor("#A973F9")
-        .setThumbnail(message.author.avatarURL)
-        .addField(message.author.username + " wanted to say", usermsg);
+function sendEmbed(message) {
+    let command = message.content;
+    let channel = message.channel;
+    let author = message.author;
 
-        message.delete().catch();
-        message.channel.send(Embedmsg);
-    }
+    // get title string coordinates in command
+    let titleStart = command.indexOf('[');
+    let titleEnd = command.indexOf(']');
+    let title = command.substr(titleStart + 1, titleEnd - titleStart - 1);
 
-module.exports.help = {
-    name: "embed"
+    // get description string coordinates in command
+    // -> (start after +1 so we don't count '[' or ']' twice)
+    let descStart = command.indexOf('[', titleStart + 1);
+    let descEnd = command.indexOf(']', titleEnd + 1);
+    let description = command.substr(descStart + 1, descEnd - descStart - 1);
+
+    // next create rich embed
+    let embed = new Discord.RichEmbed({
+        title: title,
+        description: description
+    });
+
+    // set author based of passed-in message
+    embed.setAuthor(author.username, author.displayAvatarURL);
+
+    // send embed to channel
+    channel.send(embed);
 }
