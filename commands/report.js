d984485152fca2fde6c548acc0d10b07961ddc16
LazyyPimp <54757674+LazyyPimp@users.noreply.github.com>
2019-09-02 19:23:16 -0700

    Update report.js

--- commands/report.js
+++ commands/report.js
@@ -1,8 +1,14 @@
+const talkedRecently = new Set();
 const Discord = require("discord.js");
 const errors = require("../utils/errors.js");
 let colorp = require("../colorpreset.json");
 
 module.exports.run = async (bot, message, args) => {
+    
+     if (talkedRecently.has(message.author.id)) {
+            message.channel.send("Wait 1 minute before getting typing this again. - " + message.author);
+    } else {
+    
     let rUser = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
     if(!rUser) return errors.cantfindUser(message.channel);
     let rreason = args.join(" ").slice(22);
@@ -27,6 +33,16 @@ module.exports.run = async (bot, message, args) => {
 
     message.delete().catch(O_o=>{});
     reportschannel.send(reportEmbed);
+        
+         // the user can type the command ... your command code goes here :)
+
+        // Adds the user to the set so that they can't talk for a minute
+        talkedRecently.add(message.author.id);
+        setTimeout(() => {
+          // Removes the user from the set after a minute
+          talkedRecently.delete(message.author.id);
+        }, 60000);
+    }
 
 }
 
