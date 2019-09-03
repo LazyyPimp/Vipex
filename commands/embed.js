5335c16e919ed8a97002ab634e5365409847a852
LazyyPimp <54757674+LazyyPimp@users.noreply.github.com>
2019-09-02 19:11:00 -0700

    Update embed.js

--- commands/embed.js
+++ commands/embed.js
@@ -1,3 +1,6 @@
+const talkedRecently = new Set();
+
+
 module.exports.run = async (bot, message , args, prefix) => {
 
  if(!message.member.hasPermission("MANAGE_MESSAGES")) return message.reply("You dont have such permission.").then(msg => msg.delete(3000));
@@ -9,6 +12,20 @@ module.exports.run = async (bot, message , args, prefix) => {
     .setDescription(word)
     .setColor(0x00A2E8);
   message.channel.send({embed});
+ 
+  if (talkedRecently.has(msg.author.id)) {
+            msg.channel.send("Wait 1 minute before getting typing this again. - " + msg.author);
+    } else {
+
+           // the user can type the command ... your command code goes here :)
+
+        // Adds the user to the set so that they can't talk for a minute
+        talkedRecently.add(msg.author.id);
+        setTimeout(() => {
+          // Removes the user from the set after a minute
+          talkedRecently.delete(msg.author.id);
+        }, 60000);
+    }
 }
 
 module.exports.help = {
