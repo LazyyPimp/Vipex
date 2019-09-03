e1d559ee82afba78e37bf2819781e7fba17b96e4
LazyyPimp <54757674+LazyyPimp@users.noreply.github.com>
2019-09-02 18:44:01 -0700

    Update embed.js

--- commands/embed.js
+++ commands/embed.js
@@ -2,9 +2,7 @@ module.exports.run = async (bot, message , args, prefix) => {
 
  if(!message.member.hasPermission("MANAGE_MESSAGES")) return message.reply("You dont have such permission.").then(msg => msg.delete(3000));
 
-const Discord = require("discord.js");
-const bot = new Discord.Client();
-exports.run = (client, message, args) => {
+ const Discord = require("discord.js");
   const word = args.join(" ")
   if (word < 1) return message.channel.send("Didn't provide any text to embed")
   const embed = new Discord.RichEmbed()
