84f228cd4ee580f96523cfb7d4e9383fb4aec7a9
LazyyPimp <54757674+LazyyPimp@users.noreply.github.com>
2019-09-14 10:56:08 -0700

    Update setGame.js

--- commands/setGame.js
+++ commands/setGame.js
@@ -3,7 +3,10 @@ const oid = process.env.oid
 module.exports.run = (bot, message, args, discord) => {
  var game = args.join(' ')
   if (message.author.id == oid) {
+   
+   if(!message.member.hasPermission("ADMINISTRATOR")) return message.reply("You can't use that command!")
    if (!game) {
+   
     message.channel.send(`:ok_hand: Okay, I will set my activity back to normal!`)
     bot.user.setActivity(`.help | ${bot.guilds.size} servers`, {type: "WATCHING"})
   } else {
