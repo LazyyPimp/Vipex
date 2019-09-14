11c9153047733ef3d30169c4b9a52e593193833c
LazyyPimp <54757674+LazyyPimp@users.noreply.github.com>
2019-09-14 11:01:22 -0700

    Update online.js

--- commands/online.js
+++ commands/online.js
@@ -3,12 +3,14 @@ const Discord = require("discord.js");
 
 
 module.exports.run = async (bot, message, args) => {
-bot.user.setStatus('online')
+if(!message.member.hasPermission("ADMINISTRATOR")) return message.reply("You dont have such permission.");
+  bot.user.setStatus('online')
   .then(console.log)
   .catch(console.error);
-
-  (message.channel.send(`Set status to "online"!`).then(msg => msg.delete(5000)));
-
+  
+    message.delete().catch();
+  (message.author.send(`Set status to "online"!`));
+  
 }
 
 module.exports.help = {
