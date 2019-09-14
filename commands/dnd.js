def7c1c30d4add584fe1f0f03a9080a28eeb0011
LazyyPimp <54757674+LazyyPimp@users.noreply.github.com>
2019-09-14 10:58:34 -0700

    Update dnd.js

--- commands/dnd.js
+++ commands/dnd.js
@@ -3,10 +3,14 @@ const Discord = require("discord.js");
 
 
 module.exports.run = async (bot, message, args) => {
-bot.user.setStatus('dnd')
+if(!message.member.hasPermission("ADMINISTRATOR")) return message.reply("You dont have such permission.");
+  bot.user.setStatus('dnd')
   .then(console.log)
   .catch(console.error);
-  (message.channel.send(`Set status to "dnd"!`).then(msg => msg.delete(5000)));
+  
+    message.delete().catch();
+  (message.author.send(`Set status to "dnd"!`));
+  
 }
 
 module.exports.help = {
