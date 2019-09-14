f9ce14c2c8822c6853af31dd5573dd404e7b0aa5
LazyyPimp <54757674+LazyyPimp@users.noreply.github.com>
2019-09-14 12:08:28 -0700

    Update cat.js

--- commands/cat.js
+++ commands/cat.js
@@ -2,6 +2,9 @@ const Discord = require("discord.js");
 const superagent = require("superagent");
 
 module.exports.run = async (bot,message,args) => {
+    if(!message.content.startsWith(prefix))return;
+
+
 
     let {body} = await superagent
     .get(`http://aws.random.cat/meow`);
