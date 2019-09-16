bf063e9bc8b3dc0517993b3b243ba72872cdb724
LazyyPimp <54757674+LazyyPimp@users.noreply.github.com>
2019-09-16 00:13:15 -0700

    Update say.js

--- commands/say.js
+++ commands/say.js
@@ -1,6 +1,9 @@
-const Discord = require("discord.js");
+const botconfig = require("../botconfig.json");
+const prefix = botconfig.prefix
+const Discord = module.require('discord.js');
 
-module.exports.run = async (bot,message,args) => {
+module.exports.run = async (bot, message, args) => {
+if(!message.content.startsWith(prefix))return;
     
     if(!message.member.hasPermission("MANAGE_MESSAGES")) return message.reply("You dont have such permission.");
     else if(!args[0]) return message.reply("What you want me to say?")
