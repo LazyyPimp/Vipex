42f75b3c17ba147ce8989e2991d4b49cc3ea01a8
LazyyPimp <54757674+LazyyPimp@users.noreply.github.com>
2019-09-16 00:16:41 -0700

    Update vaporwave.js

--- commands/vaporwave.js
+++ commands/vaporwave.js
@@ -1,7 +1,9 @@
-const Discord = require("discord.js");
-
+const botconfig = require("../botconfig.json");
+const prefix = botconfig.prefix
+const Discord = module.require('discord.js');
 
 module.exports.run = async (bot, message, args) => {
+if(!message.content.startsWith(prefix))return;
 
 if(!args[0]) return message.reply("What you want me to say?");
 
