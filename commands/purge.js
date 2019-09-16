cbdb456fd5fd584d3e41b8666e6dda0259becac6
LazyyPimp <54757674+LazyyPimp@users.noreply.github.com>
2019-09-16 00:22:29 -0700

    Create purge.js

---
+++ commands/purge.js
@@ -0,0 +1,17 @@
+const Discord = require("discord.js");
+const botconfig = require("../botconfig.json");
+const superagent = require("superagent");
+const prefix = botconfig.prefix
+
+module.exports.run = async (bot,message,args) => {
+    
+    const deleteCount = parseInt(args[0], 10);
+    
+    if(!deleteCount || deleteCount < 2 || deleteCount > 100)
+      return message.reply("Please provide a number between 2 and 100 for the number of messages to delete");
+
+    const fetched = await message.channel.fetchMessages({limit: deleteCount});
+    message.channel.bulkDelete(fetched)
+      .catch(error => message.reply(`Couldn't delete messages because of: ${error}`));
+  }
+});
