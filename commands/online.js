7b47231796f9c22b1536c3f40b7ae5a96662574e
LazyyPimp <54757674+LazyyPimp@users.noreply.github.com>
2019-09-16 00:09:02 -0700

    Update online.js

--- commands/online.js
+++ commands/online.js
@@ -1,8 +1,10 @@
-const Discord = require("discord.js");
-
-
+const botconfig = require("../botconfig.json");
+const prefix = botconfig.prefix
+const Discord = module.require('discord.js');
 
 module.exports.run = async (bot, message, args) => {
+if(!message.content.startsWith(prefix))return;
+  
 if(!message.member.hasPermission("ADMINISTRATOR")) return message.reply("You dont have such permission.");
   bot.user.setStatus('online')
   .then(console.log)
