d1a891ba5478359a02b373d931bda3c82e7204f7
LazyyPimp <54757674+LazyyPimp@users.noreply.github.com>
2019-09-16 00:33:28 -0700

    Create ping.js

---
+++ commands/ping.js
@@ -0,0 +1,10 @@
+const botconfig = require("../botconfig.json");
+const prefix = botconfig.prefix
+const Discord = module.require('discord.js');
+
+module.exports.run = async (bot, message, args) => {
+if(!message.content.startsWith(prefix))return;
+
+const msg = await message.channel.send(`🏓 Pinging....`);
+
+        msg.edit(`🏓 Pong!\nLatency is ${Math.floor(msg.createdTimestap - message.createdTimestap)}ms\nAPI Latency is ${Math.round(bot.ping)}ms`);
