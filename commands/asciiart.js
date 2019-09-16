4a1fcbd22db60c413f071e3d6247b8378224ca19
LazyyPimp <54757674+LazyyPimp@users.noreply.github.com>
2019-09-15 23:59:04 -0700

    Update asciiart.js

--- commands/asciiart.js
+++ commands/asciiart.js
@@ -1,8 +1,9 @@
-
+const botconfig = require("../botconfig.json");
+const prefix = botconfig.prefix
 const Discord = module.require('discord.js');
 
 module.exports.run = async (bot, message, args) => {
-
+if(!message.content.startsWith(prefix))return;
     let asciiart = [
 `
 ░░░░░░░░░░░░░░░░░▄▄▄▄▄▄
