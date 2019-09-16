51ff4afca18330d8ab101e4d5ca0caa874c4a5f4
LazyyPimp <54757674+LazyyPimp@users.noreply.github.com>
2019-09-16 00:07:18 -0700

    Update flip.js

--- commands/flip.js
+++ commands/flip.js
@@ -1,6 +1,9 @@
+const botconfig = require("../botconfig.json");
+const prefix = botconfig.prefix
 const Discord = module.require('discord.js');
 
 module.exports.run = async (bot, message, args) => {
+if(!message.content.startsWith(prefix))return;
 
     let result = Math.floor((Math.random() * 2) + 1);
     	if (result == 1) {
