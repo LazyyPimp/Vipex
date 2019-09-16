b28513bf9e6d1d6490ac1771e03d4cf2ab16543f
LazyyPimp <54757674+LazyyPimp@users.noreply.github.com>
2019-09-16 00:07:42 -0700

    Update hi.js

--- commands/hi.js
+++ commands/hi.js
@@ -1,6 +1,9 @@
-const Discord = require("discord.js");
+const botconfig = require("../botconfig.json");
+const prefix = botconfig.prefix
+const Discord = module.require('discord.js');
 
 module.exports.run = async (bot, message, args) => {
+if(!message.content.startsWith(prefix))return;
         let quote = ["Hi, my name is jeff!",
                      "( ͡° ͜ʖ ͡°)",
                      "Meow~.",
