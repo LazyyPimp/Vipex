addc594c85588582464a2abfbd52c5a48a1e55a3
LazyyPimp <54757674+LazyyPimp@users.noreply.github.com>
2019-09-16 00:09:11 -0700

    Update luckycolor.js

--- commands/luckycolor.js
+++ commands/luckycolor.js
@@ -1,6 +1,9 @@
+const botconfig = require("../botconfig.json");
+const prefix = botconfig.prefix
 const Discord = module.require('discord.js');
 
-module.exports.run = async (bot, message, args, prefix) => {
+module.exports.run = async (bot, message, args) => {
+if(!message.content.startsWith(prefix))return;
 
 
     let color=''; 
