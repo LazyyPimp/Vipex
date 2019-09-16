8533bd96956a1ae7cb40edd1114a965afec3dc67
LazyyPimp <54757674+LazyyPimp@users.noreply.github.com>
2019-09-16 00:16:28 -0700

    Update userinfo.js

--- commands/userinfo.js
+++ commands/userinfo.js
@@ -1,6 +1,10 @@
+const botconfig = require("../botconfig.json");
+const prefix = botconfig.prefix
 const Discord = module.require('discord.js');
 
 module.exports.run = async (bot, message, args) => {
+if(!message.content.startsWith(prefix))return;
+  
   let memberInfo = message.mentions.members.first();
 
   if(!memberInfo){
